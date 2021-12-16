a3c59879690137b28ed3903d7a35557b62e01fd1
sz0ocsfer <sz0ocsfer@vasvari.org>
2021-12-16 12:28:22 +0100

    ProgramVersion fejlesztése

---
+++ Desktop/src/VizsgaremekTestsProgramInfo/Models/ProgramInfoTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Vizsgaremek.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vizsgaremek.Models.Tests
+{
+    [TestClass()]
+    public class ProgramInfoTests
+    {
+        [TestMethod()]
+        public void ProgramInfoTest()
+        {
+            //arrange
+            ProgramInfo programInfo = new ProgramInfo();
+            string expected = "0.0.2";
+
+            //act
+            string actual = programInfo.Version;
+
+            //assert
+            Assert.AreEqual(expected, actual, "Version is not 0.0.2");
+        }
+    }
+}
